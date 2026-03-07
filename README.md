d0e1732e965edf86f0d789fa151649f5e6d67440
Jeeman789 <gavin.r.lacy@gmail.com>
2026-03-06 20:03:31 -0500

    Started with a simple request program

---
+++ README.md
@@ -0,0 +1,2 @@
+# AI_tool
+A model used to test, experiment, and get to know how exactly I can use this to my benefit. May be used as a productivity tool in the future
---
+++ core.py
@@ -0,0 +1,16 @@
+import requests
+
+def ask(prompt):
+    response = requests.post(
+        "http://localhost:11434/api/generate",
+        json={
+            "model": "llama3",
+            "prompt": prompt,
+            "stream": False
+        }
+    )
+    return response.json()["response"]
+
+while True:
+    user = input("You: ")
+    print("AI:", ask(user))
